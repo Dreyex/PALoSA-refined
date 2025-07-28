fcf26dcc6f2e241c9d1dfc24627a8de5ac921b16
Dreyex <mrwolff103@gmail.com>
2025-07-28 10:59:21 +0200

    creating JSON Variable for Settings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,66 +1,97 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 //Icons
 
-
 //Components
-import FileUploadCard from './components/FileUploadCard';
-import SettingField from './components/SettingField';
-import Button from './components/ui/Button';
+import FileUploadCard from "./components/FileUploadCard";
+import SettingField from "./components/SettingField";
+import Button from "./components/ui/Button";
 
 function App() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    // The vite proxy will redirect this to http://localhost:3001/api/portfolio
-    fetch('/api')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        setData(data);
-        setLoading(false);
-      })
-      .catch(error => {
-        setError(error.message);
-        setLoading(false);
-      });
-  }, []); // Empty dependency array means this effect runs once on mount
+    const settingsJSON = {
+        logSettings: { checkedOptions: [] },
+        jsonSettings: { patterns: [], checkedOptions: [] },
+        xmlSettings: { patterns: [], checkedOptions: [] },
+        regexSettings: { patterns: [] },
+    };
 
-  if (loading) return <div className="text-center p-10">Loading...</div>;
-  if (error) return <div className="text-center p-10 text-rose-500">Error: {error}</div>;
+    useEffect(() => {
+        // The vite proxy will redirect this to http://localhost:3001/api/portfolio
+        fetch("/api")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setData(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                setError(error.message);
+                setLoading(false);
+            });
+    }, []); // Empty dependency array means this effect runs once on mount
 
-  return (
-    <div className='flex flex-col [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-rose-700 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-steel-900'>
-      <h1 className='text-center p-10 text-8xl font-black text-rose-600'> {data.title} </h1>
-      <h2 className='text-center text-2xl font-bold'> 
-        <span className='text-rose-600'>P</span>seudonymisierungs-
-        <span className='text-rose-600'>A</span>pp für  
-        <span className='text-rose-600'> Lo</span>g-Dateien von 
-        <span className='text-rose-600'> S</span>erver-
-        <span className='text-rose-600'>A</span>nwendungen
-      </h2>
-      <div className='flex flex-row justify-between w-10/12 mx-auto mt-12'>
-        <SettingField options={['Option 1', 'Option 2', 'Option 3']} headline={data.settingTitles[0]}/>
-        <SettingField showFileInput={true} showTextInput={true} options={['Option 1', 'Option 2', 'Option 3']} headline={data.settingTitles[1]} fileUploadType="json"/>
-        <SettingField showFileInput={true} showTextInput={true} options={['Option 1', 'Option 2', 'Option 3']} headline={data.settingTitles[2]} fileUploadType="xml"/>
-        <SettingField showTextInput={true} headline={data.settingTitles[3]} comment="Für alle Dateien"/>
-      </div>
-      <div className='mt-12'>
-        <FileUploadCard fileUploadType="other"/>
-      </div>
-      <div className='mt-12 mx-auto text-center'>
-        <Button variant="danger" className="font-extrabold text-2xl">
-          Pseudonymisieren
-        </Button>
-      </div>
-    </div>
-  );
+    if (loading) return <div className='text-center p-10'>Loading...</div>;
+    if (error)
+        return (
+            <div className='text-center p-10 text-rose-500'>Error: {error}</div>
+        );
+
+    return (
+        <div className='flex flex-col [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-rose-700 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-steel-900'>
+            <h1 className='text-center p-10 text-8xl font-black text-rose-600'>
+                {" "}
+                {data.title}{" "}
+            </h1>
+            <h2 className='text-center text-2xl font-bold'>
+                <span className='text-rose-600'>P</span>seudonymisierungs-
+                <span className='text-rose-600'>A</span>pp für
+                <span className='text-rose-600'> Lo</span>g-Dateien von
+                <span className='text-rose-600'> S</span>erver-
+                <span className='text-rose-600'>A</span>nwendungen
+            </h2>
+            <div className='flex flex-row justify-between w-10/12 mx-auto mt-12'>
+                <SettingField
+                    options={["Option 1", "Option 2", "Option 3"]}
+                    headline={data.settingTitles[0]}
+                />
+                <SettingField
+                    showFileInput={true}
+                    showTextInput={true}
+                    options={["Option 1", "Option 2", "Option 3"]}
+                    headline={data.settingTitles[1]}
+                    fileUploadType='json'
+                />
+                <SettingField
+                    showFileInput={true}
+                    showTextInput={true}
+                    options={["Option 1", "Option 2", "Option 3"]}
+                    headline={data.settingTitles[2]}
+                    fileUploadType='xml'
+                />
+                <SettingField
+                    showTextInput={true}
+                    headline={data.settingTitles[3]}
+                    comment='Für alle Dateien'
+                />
+            </div>
+            <div className='mt-12'>
+                <FileUploadCard fileUploadType='other' />
+            </div>
+            <div className='mt-12 mx-auto text-center'>
+                <Button variant='danger' className='font-extrabold text-2xl'>
+                    Pseudonymisieren
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
