2e444ea1c79ece808b34b2eb8e37890c77110801
Dreyex <mrwolff103@gmail.com>
2025-08-04 07:21:43 +0200

    Add createOutDir utility and use in processManager
    Introduces createOutDir.js to ensure output directory exists before processing. Updates processManager to call createOutDir for the 'output' directory prior to merging settings.

--- server/utils/processManager.js
+++ server/utils/processManager.js
@@ -1,10 +1,12 @@
 import mergeSettings from "./mergeSettings.js";
+import createOutDir from "./createOutDir.js";
 
 // Process Manager for the Pseudonymization process
 export default async function startProcessManager(sessionId, data)
 {
     //console.log(data);
     //console.log(sessionId);
+    createOutDir("output");
     await mergeSettings(sessionId, data, "json");
     await mergeSettings(sessionId, data, "xml");
 }
