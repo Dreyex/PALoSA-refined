201a709b7d821fb6068c7aeeb37b117ca4f81cdf
Dreyex <mrwolff103@gmail.com>
2025-08-04 10:29:37 +0200

    Fixed and Removed unused Imports and Refactored Code in seperate Files

--- server/utils/processLogFiles.js
+++ server/utils/processLogFiles.js
@@ -1,8 +1,6 @@
 export default async function processLogFiles(uploadDir, outputDir, settings) {
     const patterns = await requestRegex(settings);
     console.log(patterns);
-
-
 }
 
 async function requestRegex(settings) {
@@ -40,22 +38,5 @@ async function requestRegex(settings) {
     } catch (error) {
         console.error("Error in requestRegex:", error);
         return [];
-    }
-}
-
-async function generateFileName(fileName) {
-    try {
-        // Insert "-pseudo" before the file extension
-        const extIndex = fileName.lastIndexOf(".");
-        let newName;
-        if (extIndex !== -1) {
-            newName = fileName.slice(0, extIndex) + "-pseudo" + fileName.slice(extIndex);
-        } else {
-            newName = fileName + "-pseudo";
-        }
-        return newName;
-    } catch (error) {
-        console.error("Error in generateFileName:", error);
-        return fileName;
     }
 }
--- server/utils/processManager.js
+++ server/utils/processManager.js
@@ -2,9 +2,9 @@ import mergeSettings from "./mergeSettings.js";
 import createOutDir from "./createOutDir.js";
 import processLogFiles from "./processLogFiles.js";
 
-import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import copyFileToOutput from "./copyFileToOutput.js";
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -15,7 +15,7 @@ export default async function startProcessManager(sessionId, data)
 {
     //Setting paths
     console.log("Setting up paths...");
-    const uploadDir = path.join(__dirname, '..', "uploads", sessionId);
+    const uploadDir = path.join(__dirname, '..', "uploads", sessionId, "other");
     const outputDir = path.join(__dirname, '..', "output", sessionId);
 
     //Creating Directorys for Output
@@ -27,6 +27,10 @@ export default async function startProcessManager(sessionId, data)
     console.log("Merging settings...");
     await mergeSettings(sessionId, data, "json");
     await mergeSettings(sessionId, data, "xml");
+
+    //Copying files to Output Directory
+    console.log("Copying Files to Output Directory...")
+    await copyFileToOutput(uploadDir, outputDir);
 
     //Processing Log Files
     console.log("Processing log files...");
